9e35071ee641888afde623835579b3fcedb36597
Yiding He <yiding.he@gmail.com>
2017-07-17 19:48:25 +0800

    SpringBoot initialization

---
+++ src/main/java/com/hyd/salto/SaltoApplication.java
@@ -0,0 +1,18 @@
+package com.hyd.salto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * (description)
+ * created at 2017/7/17
+ *
+ * @author yidin
+ */
+@SpringBootApplication
+public class SaltoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SaltoApplication.class);
+    }
+}
